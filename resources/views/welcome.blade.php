9b2e9d17915fe7aa14544f030fd3c8fc3e5533b8
Meike <mart0143@hz.nl>
2024-06-11 21:32:58 +0200

    Changed the home and about page to use the same layout as the login

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,14 +1,20 @@
-<x-layout.main>
-    <div class="navbar">
-        <div class="navbar-start">
-            <div class="block">
-                <h1 class="title is-4">Welcome to the DevelopITEasy website!</h1>
-                <h2 class="subtitle is-6 is-italic">
-                    This website was designed specifically to use for the assignments given for the
-                    IT Development Portfolio. This is part of a course called Framework Project 2, in
-                    the first year of the HBO-ICT programme at HZ.
-                </h2>
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            Welcome to the DevelopITEasy website!
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    <p> This website was designed specifically to use for the assignments given for the
+                        IT Development Portfolio. This is part of a course called Framework Project 2, in
+                        the first year of the HBO-ICT programme at HZ.
+                    </p>
+                </div>
             </div>
         </div>
     </div>
-</x-layout.main>
+</x-app-layout>
