f79946af9b8489a0831ef1c132a49b4f777f00a8
Meike <mart0143@hz.nl>
2024-06-24 20:52:15 +0200

    basic 500 error page implemented

---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,11 @@
+<x-guest-layout>
+    <h1 class="font-semibold text-2xl text-gray-800 leading-tight">500</h1>
+    <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        Internal Server Error
+    </h2>
+    <br>
+    <p> Our apologies for the inconvenience, we are working on resolving the issue.</p>
+    <br>
+    <a type="button" href="{{ route('home') }}" class="bg-gray-200
+        rounded-lg py-2 px-4 flex flex-col sm:justify-center items-center">Back to the homepage</a>
+</x-guest-layout>
